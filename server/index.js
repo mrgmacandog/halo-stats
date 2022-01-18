e74b1646d3a3ca2f40833859c0d02ace10cd75a8
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2022-01-17 21:47:47 -0800

    Update path to client/build/index.html in server

--- server/index.js
+++ server/index.js
@@ -9,15 +9,17 @@ dotenv.config();
 const __dirname = path.resolve();
 const app = express();
 
+const index = path.join(__dirname, "client", "build", "index.html");
+
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(index));
 
 // Handle GET requests to /api route
 app.use("/api", routes(app, express));
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(index);
 });
 
 app.listen(PORT, () => {
